e41d13010fa208188f67dfc6f621db5d29bf19fc
Dani M <dani.mancebo_3@hotmail.com>
2015-10-05 21:54:02 +0200

    Minor fixes in Pom file and Springfield scraper

--- Web Scraper/project/CuokaWebScraperRESTClient/src/main/java/es/sidelab/cuokawebscraperrestclient/scrapers/SpringfieldScraper.java
+++ Web Scraper/project/CuokaWebScraperRESTClient/src/main/java/es/sidelab/cuokawebscraperrestclient/scrapers/SpringfieldScraper.java
@@ -31,14 +31,14 @@ public class SpringfieldScraper implements GenericScraper
         
         try {
             // Obtener el HTML
-            Document document = Jsoup.connect( urlSection.toString() ).ignoreHttpErrors( true ).get();
+            Document document = Jsoup.connect( urlSection.toString() ).get();
             
             // Obtener el link de 'Ver todos'
             Element seeAll = document.select( "div.pagination a" ).last();
             
             // Comprobar que existe el link de 'Ver todos'
             if ( seeAll != null )
-                document = Jsoup.connect( urlShop.toString() + seeAll.attr( "href" ) ).ignoreHttpErrors( true ).get();            
+                document = Jsoup.connect( urlShop.toString() + seeAll.attr( "href" ) ).get();            
             
             // Obtener el campo info de todos los productos
             Elements products = document.select( "ul.product-listing li div div.content_product > a" );
