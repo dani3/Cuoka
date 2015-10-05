1b4a54060b316ef23e91457f9bf592a60a80e8aa
Dani M <dani.mancebo_3@hotmail.com>
2015-10-05 18:18:12 +0200

    ScraperManager added and working!!

--- Web Scraper/project/CuokaWebScraperRESTClient/src/main/java/es/sidelab/cuokawebscraperrestclient/main.java
+++ Web Scraper/project/CuokaWebScraperRESTClient/src/main/java/es/sidelab/cuokawebscraperrestclient/main.java
@@ -1,13 +1,15 @@
 package es.sidelab.cuokawebscraperrestclient;
 
+import es.sidelab.cuokawebscraperrestclient.scrapers.GenericScraper;
+import es.sidelab.cuokawebscraperrestclient.scrapers.ScraperManager;
 import es.sidelab.cuokawebscraperrestclient.scrapers.SpringfieldScraper;
 import java.net.URL;
 
 public class main 
 {
     public static void main( String[] args ) throws Exception
-    {
-        SpringfieldScraper spf = new SpringfieldScraper();
+    {        
+        GenericScraper spf = ScraperManager.getScraper( "Springfield" );
         spf.scrap( new URL( "http://spf.com/" ) , new URL( "http://spf.com/es/tienda/woman/kimonos-y-ponchos" ) );
     }    
 }
--- Web Scraper/project/CuokaWebScraperRESTClient/src/main/java/es/sidelab/cuokawebscraperrestclient/scrapers/ScraperManager.java
+++ Web Scraper/project/CuokaWebScraperRESTClient/src/main/java/es/sidelab/cuokawebscraperrestclient/scrapers/ScraperManager.java
@@ -1,9 +1,31 @@
 package es.sidelab.cuokawebscraperrestclient.scrapers;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * @Class Clase que gestiona los distintos scrapers, actua de middleware.
  * @author Daniel Mancebo Aldea
  */
-public class ScraperManager {
-    
+public class ScraperManager 
+{
+    public static GenericScraper getScraper( String shop ) 
+    {
+        try {
+            return ( GenericScraper ) Class.forName( "es.sidelab.cuokawebscraperrestclient.scrapers." 
+                                        + shop + "Scraper" ).newInstance();
+            
+        } catch ( ClassNotFoundException ex ) {
+            Logger.getLogger(ScraperManager.class.getName()).log(Level.SEVERE, null, ex);
+            
+        } catch ( InstantiationException ex ) {
+            Logger.getLogger(ScraperManager.class.getName()).log(Level.SEVERE, null, ex);
+            
+        } catch ( IllegalAccessException ex ) {
+            Logger.getLogger(ScraperManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        // No debería llegar aquí
+        return null;
+    }
 }
