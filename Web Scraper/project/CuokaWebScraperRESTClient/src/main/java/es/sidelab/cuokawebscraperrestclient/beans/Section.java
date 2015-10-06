08665d78a721d2a9453f65213f01301600448ff9
Daniel Mancebo <dani.mancebo_3@hotmail.com>
2015-10-06 20:33:51 +0200

    Revert "RestServer"

--- Web Scraper/project/CuokaWebScraperRESTClient/src/main/java/es/sidelab/cuokawebscraperrestclient/beans/Section.java
+++ Web Scraper/project/CuokaWebScraperRESTClient/src/main/java/es/sidelab/cuokawebscraperrestclient/beans/Section.java
@@ -1,7 +1,5 @@
 package es.sidelab.cuokawebscraperrestclient.beans;
 
-import java.net.URL;
-
 /**
  * @class Clase que representa una sección de una tienda, contendrá su URL,
  *        el nombre y el sexo.
@@ -10,15 +8,5 @@ import java.net.URL;
  */
 public class Section 
 {
-    private String _name;
-    private URL _url;
-
-    public Section( String name, URL url ) 
-    {
-        this._name = name;
-        this._url = url;
-    }
     
-    public String getName() { return _name; }
-    public URL getURL() { return _url; }
 }
