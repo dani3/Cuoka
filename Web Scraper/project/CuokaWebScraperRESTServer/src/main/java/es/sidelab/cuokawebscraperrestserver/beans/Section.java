d9d85c71fc121da0205c0670a7a0cfbf6224c391
Dani M <dani.mancebo_3@hotmail.com>
2015-10-06 20:38:04 +0200

    Components annotated

--- Web Scraper/project/CuokaWebScraperRESTServer/src/main/java/es/sidelab/cuokawebscraperrestserver/beans/Section.java
+++ Web Scraper/project/CuokaWebScraperRESTServer/src/main/java/es/sidelab/cuokawebscraperrestserver/beans/Section.java
@@ -1,9 +1,12 @@
 package es.sidelab.cuokawebscraperrestserver.beans;
 
+import org.springframework.stereotype.Component;
+
 /**
  * @class Clase que representa una categoria de una tienda, contiene el nombre y la URL
  * @author Daniel Mancebo Aldea
  */
+@Component
 public class Section 
 {
     
--- Web Scraper/project/CuokaWebScraperRESTServer/src/main/java/es/sidelab/cuokawebscraperrestserver/controller/ShopController.java
+++ Web Scraper/project/CuokaWebScraperRESTServer/src/main/java/es/sidelab/cuokawebscraperrestserver/controller/ShopController.java
@@ -1,9 +1,12 @@
 package es.sidelab.cuokawebscraperrestserver.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @class Controlador que maneja las peticiones entrantes
  * @author Daniel Mancebo Aldea
  */
+@RestController
 public class ShopController 
 {
     
