096205ed65695c161a9bfba13ebae82523621825
Dani M <dani.mancebo_3@hotmail.com>
2015-10-23 23:55:47 +0200

    Python script added to resize images downloaded

---
+++ Web Scraper/scripts/resize.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+import PIL
+import os
+from PIL import Image
+
+for root, dirs, files in os.walk('images/'):
+	for folder in dirs:
+		for fname in os.listdir('images/' + folder):
+			img = Image.open('images/' + folder + '/' + fname)
+			img = img.resize((350,500), PIL.Image.ANTIALIAS)
+			img.save('images/' + folder + '/' + fname)
