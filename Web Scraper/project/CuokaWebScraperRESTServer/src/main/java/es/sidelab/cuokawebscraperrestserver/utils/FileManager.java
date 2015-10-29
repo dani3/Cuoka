0e31f4e3e0d5e2697567e30a47dcbda5493a949c
Dani M <dani.mancebo_3@hotmail.com>
2015-10-29 17:11:48 +0100

    FileManager class added

---
+++ Web Scraper/project/CuokaWebScraperRESTServer/src/main/java/es/sidelab/cuokawebscraperrestserver/utils/FileManager.java
@@ -0,0 +1,11 @@
+package es.sidelab.cuokawebscraperrestserver.utils;
+
+/**
+ * @class Clase que gestionara todo lo relacionado con directorios y ficheros
+ * @author Daniel Mancebo Aldea
+ */
+
+public class FileManager 
+{
+    
+}
