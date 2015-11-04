1294b8150205fb0dce71ddc991c9b115e99921f6
Dani M <dani.mancebo_3@hotmail.com>
2015-11-04 17:20:42 +0100

    Some configuration fixes don

---
+++ Web Scraper/project/CuokaWebScraperRESTClient/src/main/java/es/sidelab/cuokawebscraperrestclient/beans/Size.java
@@ -0,0 +1,26 @@
+package es.sidelab.cuokawebscraperrestclient.beans;
+
+/**
+ * @class Clase que representa una talla
+ * @author Daniel Mancebo Aldea
+ */
+
+public class Size 
+{
+    private String size;
+    private boolean stock;
+    
+    public Size() {}
+    
+    public Size( String size, boolean stock )
+    {
+        this.size = size;
+        this.stock = stock;
+    }
+    
+    public String getSize() { return this.size; }
+    public boolean hasStock() { return this.stock; }
+    
+    public void setSize( String size ) { this.size = size; }
+    public void setStock( boolean stock ) { this.stock = stock; }
+}
