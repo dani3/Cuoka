58aa9018bc32b226ba5497237de24679f3c8e862
Dani M <dani.mancebo_3@hotmail.com>
2015-11-09 19:08:31 +0100

    Portrait orientation forced in every activity

--- WallaKoala/app/src/main/java/com/wallakoala/wallakoala/Activities/IntroUI.java
+++ WallaKoala/app/src/main/java/com/wallakoala/wallakoala/Activities/IntroUI.java
@@ -1,7 +1,9 @@
-package com.wallakoala.wallakoala;
+package com.wallakoala.wallakoala.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.wallakoala.wallakoala.R;
 
 /**
  * Created by Daniel Mancebo on 09/11/2015.
--- WallaKoala/app/src/main/java/com/wallakoala/wallakoala/Activities/LoginUI.java
+++ WallaKoala/app/src/main/java/com/wallakoala/wallakoala/Activities/LoginUI.java
@@ -1,18 +1,20 @@
-package com.wallakoala.wallakoala;
+package com.wallakoala.wallakoala.Activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import com.wallakoala.wallakoala.R;
 
 /**
  * Created by Daniel Mancebo on 09/11/2015.
  */
 
-public class ProductsUI extends AppCompatActivity
+public class LoginUI extends AppCompatActivity
 {
     @Override
     protected void onCreate( Bundle savedInstanceState )
     {
         super.onCreate( savedInstanceState );
-        setContentView( R.layout.products );
+        setContentView( R.layout.login );
     }
 }
