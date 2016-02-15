de0df7ccb792892a3579d27605ea43b7f41c83a4
Daniel Mancebo <dani.mancebo_3@hotmail.com>
2016-02-15 20:42:34 +0100

    Python scripts now ignore exceptions

--- Web Scraper/scripts/resizeProducts.py
+++ Web Scraper/scripts/resizeProducts.py
@@ -18,11 +18,14 @@ ImageFile.LOAD_TRUNCATED_FILES = True
 
 for fname in os.listdir(path):
     if 'Small' not in fname and 'ICON' not in fname and 'Large' not in fname:
-        img = Image.open(path + '/' + fname)
-        img_s = img.resize((width_s,height_s), PIL.Image.NEAREST)
-        img_s.save(path + '/' + fname.replace(".jpg", "_Small.jpg"))
+        try:
+            img = Image.open(path + '/' + fname)
+            img_s = img.resize((width_s,height_s), PIL.Image.NEAREST)
+            img_s.save(path + '/' + fname.replace(".jpg", "_Small.jpg"))
 
-        img_l = img.resize((width_l,height_l), PIL.Image.NEAREST)
-        img_l.save(path + '/' + fname.replace(".jpg", "_Large.jpg"))
+            img_l = img.resize((width_l,height_l), PIL.Image.NEAREST)
+            img_l.save(path + '/' + fname.replace(".jpg", "_Large.jpg"))
 
-        os.remove(path + '/' + fname)
+            os.remove(path + '/' + fname)
+        except Exception:
+            pass
