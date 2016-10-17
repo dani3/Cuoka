a22c97bc3939863366fb97e7cf31f97dd3a5d155
Daniel Mancebo <dani.mancebo_3@hotmail.com>
2016-10-17 20:42:02 +0200

    RestClient fixed

--- WebScraper/project/CuokaWebScraperRESTClient/src/main/java/es/sidelab/cuokawebscraperrestclient/utils/RestClient.java
+++ WebScraper/project/CuokaWebScraperRESTClient/src/main/java/es/sidelab/cuokawebscraperrestclient/utils/RestClient.java
@@ -16,23 +16,23 @@ public class RestClient
 {
     private static final Logger LOG = Logger.getLogger(RestClient.class);
     
-    private static RestTemplate restClient;
-    private static URL SERVER;
+    private RestTemplate restClient;
+    private URL server;
     
     public RestClient(URL server) 
     {
-        RestClient.restClient = new RestTemplate();
-        RestClient.SERVER = server;
+        this.restClient = new RestTemplate();
+        this.server = server;
     }
     
     /*
      * Metodo que envia una lista de productos al servidor REST.
      */
-    public static synchronized void saveProducts(List<Product> products, Shop shop)
+    public synchronized void saveProducts(List<Product> products, Shop shop)
     {
         LOG.info("Enviando lista de productos de " + shop.getName() + " al servidor...");
         LOG.info("Se envian " + products.size() + " products!");
-        restClient.postForObject(SERVER.toString() 
+        restClient.postForObject(server.toString() 
                 + "/products/" + shop.getName(), products.toArray(), Product[].class);
         LOG.info("Procuctos enviados correctamente");
     }
