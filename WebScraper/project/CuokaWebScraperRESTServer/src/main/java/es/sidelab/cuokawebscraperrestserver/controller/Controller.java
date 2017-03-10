6bb1ee076b6384271b9336c266d627b446bdd06f
Daniel Mancebo <dani.mancebo_3@hotmail.com>
2017-03-10 17:29:13 +0100

    MailManager class created

--- WebScraper/project/CuokaWebScraperRESTServer/src/main/java/es/sidelab/cuokawebscraperrestserver/controller/Controller.java
+++ WebScraper/project/CuokaWebScraperRESTServer/src/main/java/es/sidelab/cuokawebscraperrestserver/controller/Controller.java
@@ -17,8 +17,8 @@ import es.sidelab.cuokawebscraperrestserver.repositories.ShopSuggestedRepository
 import es.sidelab.cuokawebscraperrestserver.repositories.ShopsRepository;
 import es.sidelab.cuokawebscraperrestserver.repositories.UsersRepository;
 import es.sidelab.cuokawebscraperrestserver.utils.ColorManager;
-import es.sidelab.cuokawebscraperrestserver.utils.FileManager;
 import es.sidelab.cuokawebscraperrestserver.utils.ImageManager;
+import es.sidelab.cuokawebscraperrestserver.utils.MailManager;
 import es.sidelab.cuokawebscraperrestserver.utils.SectionManager;
 import es.sidelab.cuokawebscraperrestserver.utils.ShopManager;
 import es.sidelab.cuokawebscraperrestserver.utils.Utils;
@@ -32,8 +32,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -42,9 +40,6 @@ import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.mail.MailException;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -67,9 +62,6 @@ public class Controller
     
     @Autowired 
     private static final ScheduledExecutorService SCHEDULED_EXECUTOR = Executors.newScheduledThreadPool(2);
-    
-    @Autowired
-    private JavaMailSender javaMailSender;
     
     @Autowired
     private ProductsRepository productsRepository;
@@ -97,6 +89,9 @@ public class Controller
     
     @Autowired
     private ShopManager shopManager;
+    
+    @Autowired
+    private MailManager mailManager;
     
     /**
      * Metodo que añade una nueva notificacion.
@@ -261,7 +256,7 @@ public class Controller
     {
         shopSuggestedRepository.save(shopSuggested);
         
-        _sendShopSuggestion(shopSuggested.getShop(), shopSuggested.getLink());
+        mailManager.sendShopSuggestion(shopSuggested.getShop(), shopSuggested.getLink());
         
         return Properties.ACCEPTED;
     }
@@ -276,7 +271,7 @@ public class Controller
     {
         feedbackRepository.save(feedback);
         
-        _sendFeedbackEmail(feedback.getStars(), feedback.getOpinion());
+        mailManager.sendFeedbackEmail(feedback.getStars(), feedback.getOpinion());
         
         return Properties.ACCEPTED;
     }
@@ -307,7 +302,7 @@ public class Controller
             // Se guarda el usuario en BD.
             usersRepository.save(user);
             
-            _sendPasswordEmail(user.getEmail(), Properties.WELCOME_EMAIL_FROM, newPassword);
+            mailManager.sendPasswordEmail(user.getEmail(), Properties.WELCOME_EMAIL_FROM, newPassword);
         }    
         
         return Properties.ACCEPTED;
@@ -350,7 +345,7 @@ public class Controller
         
         SCHEDULED_EXECUTOR.schedule(() -> {
             LOG.info("[EMAIL] Se envia correo de bienvenida");
-            _sendWelcomeEmail(user.getEmail(), Properties.WELCOME_EMAIL_FROM, user.getName(), user.getMan());
+            mailManager.sendWelcomeEmail(user.getEmail(), Properties.WELCOME_EMAIL_FROM, user.getName(), user.getMan());
         }, 1, TimeUnit.DAYS);
         
         return String.valueOf(id);
@@ -761,7 +756,7 @@ public class Controller
                     }
                 }
                 
-                _sendScrapingStatsEmail(s.getName(), s.getProducts(), newProducts, sameProducts, obsoleteProducts);
+                mailManager.sendScrapingStatsEmail(s.getName(), s.getProducts(), newProducts, sameProducts, obsoleteProducts);
                 
             } else {         
                 boolean woman = false;
@@ -1509,169 +1504,6 @@ public class Controller
         }
         
         return false;
-    }
-    
-    /**
-     * Metodo que envia un correo al destinatario especificado.
-     * @param to: destinatario.
-     * @param from: emisor.
-     * @param name: nombre del usuario.
-     * @param man: hombre o mujer.
-     */
-    private void _sendWelcomeEmail(String to, String from, String name, boolean man)
-    {
-        MimeMessage mail = javaMailSender.createMimeMessage();
-        
-        try 
-        {
-            MimeMessageHelper helper = new MimeMessageHelper(mail, true);
-            
-            String message = FileManager.getHTMLFromFile(Properties.MAIL_PATH + Properties.WELCOME_EMAIL_NAME);
-            if (message != null)
-            {
-                message = message.replace("?1", (man ? "o" : "a"));
-                message = message.replace("?2", name);
-            }
-                
-            helper.setTo(to);
-            helper.setFrom(from);
-            helper.setSubject(Properties.WELCOME_EMAIL_SUBJECT);
-            helper.setText(message, true);
-        
-            javaMailSender.send(mail);
-            
-            LOG.info("[EMAIL] Email enviado correctamente");
-            
-        } catch (MessagingException | MailException e) {
-            LOG.error("[EMAIL] Error enviando email (" + e.getMessage() + ")");
-        }
-    }
-    
-    /**
-     * Metodo que envia un correo al destinatario especificado.
-     * @param to: destinatario.
-     * @param from: emisor.
-     */
-    private void _sendPasswordEmail(String to, String from, String password)
-    {
-        MimeMessage mail = javaMailSender.createMimeMessage();
-        
-        try 
-        {
-            MimeMessageHelper helper = new MimeMessageHelper(mail, true);
-            
-            String message = FileManager.getHTMLFromFile(Properties.MAIL_PATH + Properties.REMEMBER_PWD_EMAIL_NAME);
-            if (message != null)
-            {
-                message = message.replace("?1", password);
-            }
-                
-            helper.setTo(to);
-            helper.setFrom(from);
-            helper.setSubject(Properties.RECOVER_PASSWORD_EMAIL_SUBJECT);
-            helper.setText(message, true);
-        
-            javaMailSender.send(mail);
-            
-            LOG.info("[EMAIL] Email enviado correctamente");
-            
-        } catch (MessagingException | MailException e) {
-            LOG.error("[EMAIL] Error enviando email (" + e.getMessage() + ")");
-        }
-    }
-    
-    /**
-     * Metodo que envia un email con el feedback recibido.
-     * @param stars: numero de estrellas.
-     * @param message: mensaje.
-     */
-    private void _sendFeedbackEmail(int stars, String message)
-    {
-        MimeMessage mail = javaMailSender.createMimeMessage();
-        
-        try 
-        {
-            MimeMessageHelper helper = new MimeMessageHelper(mail);
-                
-            helper.setTo(
-                Properties.FEEDBACK_EMAIL_FROM);
-            helper.setFrom(
-                Properties.FEEDBACK_EMAIL_FROM);
-            helper.setSubject(
-                Properties.FEEDBACK_EMAIL_SUBJECT.replace("?1", Integer.toString(stars)));
-            helper.setText(
-                ((message == null || message.isEmpty()) ? "No se ha recibido ningún comentario" : message));
-        
-            javaMailSender.send(mail);
-            
-        } catch (MessagingException | MailException e) {
-            LOG.error("[EMAIL] Error enviando email (" + e.getMessage() + ")");
-        }
-    }
-    
-    /**
-     * Metodo que envia un email con la tienda sugerida.
-     * @param shop: nombre de la tienda sugerida.
-     * @param link: link de la tienda.
-     */
-    private void _sendShopSuggestion(String shop, String link)
-    {
-        MimeMessage mail = javaMailSender.createMimeMessage();
-        
-        try 
-        {
-            MimeMessageHelper helper = new MimeMessageHelper(mail);
-                
-            helper.setTo(
-                Properties.SHOP_SUGGESTION_EMAIL_FROM);
-            helper.setFrom(
-                Properties.SHOP_SUGGESTION_EMAIL_FROM);
-            helper.setSubject(
-                Properties.SHOP_SUGGESTION_EMAIL_SUBJECT);
-            helper.setText(
-                " - Nombre de la tienda: " + shop.toUpperCase() + "\n - Link: " + ((link == null || link.isEmpty()) ? "No especificado" : link));
-        
-            javaMailSender.send(mail);
-            
-        } catch (MessagingException | MailException e) {
-            LOG.error("[EMAIL] Error enviando email (" + e.getMessage() + ")");
-        }
-    }
-    
-    /**
-     * Metodo que envía un correo con las estadísticas del scraping.
-     * @param shop: nombre de la tienda.
-     * @param total: total de productos.
-     * @param newProducts: numero de productos nuevos.
-     * @param sameProducts: numero de productos iguales.
-     * @param obsoleteProducts: numero de productos obsoletos.
-     */
-    private void _sendScrapingStatsEmail(String shop, int total, int newProducts, int sameProducts, int obsoleteProducts)
-    {
-        MimeMessage mail = javaMailSender.createMimeMessage();
-        
-        try 
-        {
-            MimeMessageHelper helper = new MimeMessageHelper(mail);
-                
-            String message = " - Total de productos recibidos: " + total + "\n"
-                + " - Productos nuevos: " + newProducts + "\n"
-                + " - Productos iguales: " + sameProducts + "\n"
-                + " - Productos obsoletos: " + obsoleteProducts + "\n";
-            
-            helper.setTo(
-                Properties.SCRAPING_STATS_EMAIL_FROM);
-            helper.setFrom(
-                Properties.SCRAPING_STATS_EMAIL_FROM);
-            helper.setSubject(
-                Properties.SCRAPING_STATS_EMAIL_SUBJECT.replace("?1", shop));
-            helper.setText(message);
-        
-            javaMailSender.send(mail);
-            
-        } catch (MessagingException | MailException e) {
-            LOG.error("[EMAIL] Error enviando email (" + e.getMessage() + ")");
-        }
     }
     
     @Scheduled(cron = "0 0 8 * * ?")
---
+++ WebScraper/project/CuokaWebScraperRESTServer/src/main/java/es/sidelab/cuokawebscraperrestserver/utils/MailManager.java
@@ -0,0 +1,188 @@
+package es.sidelab.cuokawebscraperrestserver.utils;
+
+import es.sidelab.cuokawebscraperrestserver.properties.Properties;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+/**
+ * Clase que gestiona el envío de correos.
+ * @author Daniel Mancebo Aldea
+ */
+@Component
+public class MailManager 
+{   
+    private static final Log LOG = LogFactory.getLog(MailManager.class);
+    
+    @Autowired
+    private JavaMailSender javaMailSender;    
+    
+    /**
+     * Metodo que envia un correo al destinatario especificado.
+     * @param to: destinatario.
+     * @param from: emisor.
+     * @param name: nombre del usuario.
+     * @param man: hombre o mujer.
+     */
+    public void sendWelcomeEmail(String to, String from, String name, boolean man)
+    {
+        MimeMessage mail = javaMailSender.createMimeMessage();
+        
+        try 
+        {
+            MimeMessageHelper helper = new MimeMessageHelper(mail, true);
+            
+            String message = FileManager.getHTMLFromFile(Properties.MAIL_PATH + Properties.WELCOME_EMAIL_NAME);
+            if (message != null)
+            {
+                message = message.replace("?1", (man ? "o" : "a"));
+                message = message.replace("?2", name);
+            }
+                
+            helper.setTo(to);
+            helper.setFrom(from);
+            helper.setSubject(Properties.WELCOME_EMAIL_SUBJECT);
+            helper.setText(message, true);
+        
+            javaMailSender.send(mail);
+            
+            LOG.info("[EMAIL] Email enviado correctamente");
+            
+        } catch (MessagingException | MailException e) {
+            LOG.error("[EMAIL] Error enviando email (" + e.getMessage() + ")");
+        }
+    }
+    
+    /**
+     * Metodo que envia un correo al destinatario especificado.
+     * @param to: destinatario.
+     * @param from: emisor.
+     */
+    public void sendPasswordEmail(String to, String from, String password)
+    {
+        MimeMessage mail = javaMailSender.createMimeMessage();
+        
+        try 
+        {
+            MimeMessageHelper helper = new MimeMessageHelper(mail, true);
+            
+            String message = FileManager.getHTMLFromFile(Properties.MAIL_PATH + Properties.REMEMBER_PWD_EMAIL_NAME);
+            if (message != null)
+            {
+                message = message.replace("?1", password);
+            }
+                
+            helper.setTo(to);
+            helper.setFrom(from);
+            helper.setSubject(Properties.RECOVER_PASSWORD_EMAIL_SUBJECT);
+            helper.setText(message, true);
+        
+            javaMailSender.send(mail);
+            
+            LOG.info("[EMAIL] Email enviado correctamente");
+            
+        } catch (MessagingException | MailException e) {
+            LOG.error("[EMAIL] Error enviando email (" + e.getMessage() + ")");
+        }
+    }
+    
+    /**
+     * Metodo que envia un email con el feedback recibido.
+     * @param stars: numero de estrellas.
+     * @param message: mensaje.
+     */
+    public void sendFeedbackEmail(int stars, String message)
+    {
+        MimeMessage mail = javaMailSender.createMimeMessage();
+        
+        try 
+        {
+            MimeMessageHelper helper = new MimeMessageHelper(mail);
+                
+            helper.setTo(
+                Properties.FEEDBACK_EMAIL_FROM);
+            helper.setFrom(
+                Properties.FEEDBACK_EMAIL_FROM);
+            helper.setSubject(
+                Properties.FEEDBACK_EMAIL_SUBJECT.replace("?1", Integer.toString(stars)));
+            helper.setText(
+                ((message == null || message.isEmpty()) ? "No se ha recibido ningún comentario" : message));
+        
+            javaMailSender.send(mail);
+            
+        } catch (MessagingException | MailException e) {
+            LOG.error("[EMAIL] Error enviando email (" + e.getMessage() + ")");
+        }
+    }
+    
+    /**
+     * Metodo que envia un email con la tienda sugerida.
+     * @param shop: nombre de la tienda sugerida.
+     * @param link: link de la tienda.
+     */
+    public void sendShopSuggestion(String shop, String link)
+    {
+        MimeMessage mail = javaMailSender.createMimeMessage();
+        
+        try 
+        {
+            MimeMessageHelper helper = new MimeMessageHelper(mail);
+                
+            helper.setTo(
+                Properties.SHOP_SUGGESTION_EMAIL_FROM);
+            helper.setFrom(
+                Properties.SHOP_SUGGESTION_EMAIL_FROM);
+            helper.setSubject(
+                Properties.SHOP_SUGGESTION_EMAIL_SUBJECT);
+            helper.setText(
+                " - Nombre de la tienda: " + shop.toUpperCase() + "\n - Link: " + ((link == null || link.isEmpty()) ? "No especificado" : link));
+        
+            javaMailSender.send(mail);
+            
+        } catch (MessagingException | MailException e) {
+            LOG.error("[EMAIL] Error enviando email (" + e.getMessage() + ")");
+        }
+    }
+    
+    /**
+     * Metodo que envía un correo con las estadísticas del scraping.
+     * @param shop: nombre de la tienda.
+     * @param total: total de productos.
+     * @param newProducts: numero de productos nuevos.
+     * @param sameProducts: numero de productos iguales.
+     * @param obsoleteProducts: numero de productos obsoletos.
+     */
+    public void sendScrapingStatsEmail(String shop, int total, int newProducts, int sameProducts, int obsoleteProducts)
+    {
+        MimeMessage mail = javaMailSender.createMimeMessage();
+        
+        try 
+        {
+            MimeMessageHelper helper = new MimeMessageHelper(mail);
+                
+            String message = " - Total de productos recibidos: " + total + "\n"
+                + " - Productos nuevos: " + newProducts + "\n"
+                + " - Productos iguales: " + sameProducts + "\n"
+                + " - Productos obsoletos: " + obsoleteProducts + "\n";
+            
+            helper.setTo(
+                Properties.SCRAPING_STATS_EMAIL_FROM);
+            helper.setFrom(
+                Properties.SCRAPING_STATS_EMAIL_FROM);
+            helper.setSubject(
+                Properties.SCRAPING_STATS_EMAIL_SUBJECT.replace("?1", shop));
+            helper.setText(message);
+        
+            javaMailSender.send(mail);
+            
+        } catch (MessagingException | MailException e) {
+            LOG.error("[EMAIL] Error enviando email (" + e.getMessage() + ")");
+        }
+    }
+}
